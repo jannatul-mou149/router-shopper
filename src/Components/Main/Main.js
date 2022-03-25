fe1a24fbab15df2bd53bdc8a4a7ea0bb48d3d406
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-03-25 20:51:56 +0600

    api added

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css'
 const Main = () => {
-    return (
-        <div>
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className='main-container'>
+            <div className="products">
+                <h3>This is for Products: {products.length}</h3>
+            </div>
+            <div className="cart">
+                <h3>Order Summary</h3>
+            </div>
         </div>
     );
 };
